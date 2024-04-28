53d3c8ba945644a3c2f5549bee10b23c0b9ad019
loopes.nb <lopes.nb@gmail.com>
2024-04-28 09:48:51 +0900

    test case syuusei

--- TestSoya/test.cpp
+++ TestSoya/test.cpp
@@ -2,28 +2,32 @@
 #include <string>
 #include "FizzBuzz.h"
 using namespace std;
-class TestFizzBuzz : public ::testing::Test
+namespace inputNumToFizzBuzz
 {
-protected:
-	virtual void SetUp() {
+	class TestFizzBuzz : public ::testing::Test
+	{
+	protected:
+		virtual void SetUp() {
+			FizzBuzz fizzBuzz;
+		}
 		FizzBuzz fizzBuzz;
+	};
+
+	TEST_F(TestFizzBuzz, input1) {
+		EXPECT_EQ(fizzBuzz.change(1), "1");
+
 	}
-	FizzBuzz fizzBuzz;
-};
+	TEST_F(TestFizzBuzz, input3) {
+		EXPECT_EQ(fizzBuzz.change(3), "Fizz");
 
-TEST_F(TestFizzBuzz, input1) {
-	EXPECT_EQ(fizzBuzz.change(1), "1");
+	}
+	TEST_F(TestFizzBuzz, input5) {
+		EXPECT_EQ(fizzBuzz.change(5), "Buzz");
 
-}
-TEST_F(TestFizzBuzz, input3) {
-	EXPECT_EQ(fizzBuzz.change(3), "Fizz");
+	}
 
-}
-TEST_F(TestFizzBuzz, input5) {
-	EXPECT_EQ(fizzBuzz.change(5), "Buzz");
+	TEST_F(TestFizzBuzz, input15) {
+		EXPECT_EQ(fizzBuzz.change(15), "FizzBuzz");
 
-}
-TEST_F(TestFizzBuzz, input15) {
-	EXPECT_EQ(fizzBuzz.change(15), "FizzBuzz");
-
+	}
 }
