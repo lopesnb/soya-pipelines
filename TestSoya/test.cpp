068d917026670e28f46cd0c8b7a7c07f9480f999
loopes.nb <lopes.nb@gmail.com>
2024-05-08 10:09:38 +0900

    Section x y ido

--- TestSoya/test.cpp
+++ TestSoya/test.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 #include <string>
 #include "FizzBuzz.h"
-#include "XYZ.h"
+#include "Section.h"
 using namespace std;
 namespace TestInputNumToFizzBuzz
 {
@@ -34,14 +34,77 @@ namespace TestInputNumToFizzBuzz
 }
 namespace TestKeijyoIdo
 {
-	TEST(Test_XYZ, yIdo) {
-		XYZ xyz ;
-		XYZ retxyz=xyz.idoY(1);
-		EXPECT_EQ(1, retxyz.y);
-	}
-	TEST(Test_XYZ, xIdo) {
+	class TestXYZ : public ::testing::Test
+	{
+	protected:
+		virtual void SetUp() {
+			xyz = XYZ(1, 1, 1);
+		}
 		XYZ xyz;
-		XYZ retxyz = xyz.idoX(1);
-		EXPECT_EQ(1, retxyz.x);
+	};
+	TEST_F(TestXYZ, xMove) {
+
+		XYZ retxyz = xyz.moveX(1);
+		EXPECT_TRUE(XYZ(2, 1, 1) == retxyz);
 	}
+	TEST_F(TestXYZ, yMove) {
+
+		XYZ retxyz = xyz.moveY(1);
+		EXPECT_TRUE(XYZ(1, 2, 1) == retxyz);
+	}
+	TEST_F(TestXYZ, zMove) {
+		XYZ retxyz = xyz.moveZ(1);
+		EXPECT_TRUE(XYZ(1, 1, 2) == retxyz);
+	}
+	class TestXYZRot : public ::testing::Test
+	{
+	protected:
+		virtual void SetUp() {
+			xyz = XYZ(0, 0, 1);
+		}
+		XYZ xyz;
+	};
+	TEST_F(TestXYZRot, xRot) {
+		XYZ retxyz = xyz.rotX(90);
+		EXPECT_TRUE(XYZ(0, -1, 0) == retxyz);
+	}
+	TEST_F(TestXYZRot, yRot) {
+		XYZ retxyz = xyz.rotY(90);
+		EXPECT_TRUE(XYZ(1, 0, 0) == retxyz);
+	}
+	TEST_F(TestXYZRot, zRot) {
+		XYZ retxyz = xyz.rotZ(90);
+		EXPECT_TRUE(XYZ(0, 0, 1) == retxyz);
+	}
+}
+namespace TestSection
+{
+	class TestSectionF : public ::testing::Test
+	{
+	protected:
+		virtual void SetUp() {
+			for (int i = 0; i < 36; i++)
+			{
+				double radKaku=M_PI*(10.0*i / 180);
+				double x = cos(radKaku)*100;
+				double y = sin(radKaku)*100;
+				section.addXYZ(XYZ(x,y,0));
+
+			}
+		}
+		Section section;
+	};
+	TEST_F(TestSectionF, idoX)
+	{
+	
+		Section sectionMove=section.moveX(1);
+		EXPECT_TRUE(XYZ(101, 0, 0)== sectionMove.getXYZ(0));
+	}
+	TEST_F(TestSectionF, idoY)
+	{
+
+		Section sectionMove = section.moveY(1);
+		EXPECT_TRUE(XYZ(0, 101, 0) == sectionMove.getXYZ(9));
+	}
+
 }
